3b37da47300e66b3cfe48f487dcc45a0e547a86b
dddCddd <the.gargamel.organisation@gmail.com>
2020-10-28 14:46:06 +0000

    php form

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_POST ["SKICKA"])){
+    $name = $_POST ["name"];
+    $mailFrom = $_POST ["email"];
+    $message = $_POST ["message"];
+
+    $mailTo = "rickard@betydlig.se";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have reccived an e-mail from" .$name.".\n\n".$message;
+    
+    mail($mailTo, $message, $txt, $headers);
+    header("Location: index.php?mailsend");
+
+}
+
+
+
+
